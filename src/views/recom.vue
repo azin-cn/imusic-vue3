0310df2519fdf2851ad28bc1593524bfd850f7dc
luckydog <luckywords@foxmail.com>
2022-08-08 13:12:06 +0800

    搭建pages页面

---
+++ src/views/recom.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Recom</div>
+</template>
+
+<script>
+export default {
+  name: 'Recom'
+}
+</script>
+
+<script setup>
+</script>
+
+<style>
+</style>
---
+++ src/views/top.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Top</div>
+</template>
+
+<script>
+export default {
+  name: 'Top'
+}
+</script>
+
+<script setup>
+</script>
+
+<style>
+</style>
