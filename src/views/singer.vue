9542e94dfe563555ce2431c2f5e17a3b67b40421
azin-cn <luckywords@foxmail.com>
2022-08-10 15:30:18 +0800

    update

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>Singer</div>
+  <div class="singer" v-loading="!singerList.length"><IndexList :data="singerList" /></div>
 </template>
 
 <script>
 export default {
-  name: 'Singer'
-}
+  name: "Singer",
+};
 </script>
 
 <script setup>
+import { ref, onMounted } from "vue";
+import IndexList from "@/components/base/IndexList";
+import getSingerList from "@/service/singer";
+
+const singerList = ref([]);
+onMounted(async () => {
+  const result = await getSingerList();
+  singerList.value = result.singers;
+});
 </script>
 
-<style>
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
