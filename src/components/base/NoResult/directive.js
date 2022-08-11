9c2c059b0cea5ed49d84c459edcf64d760246bc8
azin-cn <luckywords@foxmail.com>
2022-08-11 11:04:33 +0800

    更新：添加createDirective类Loading的通用指令，使用Component.name作为前缀区分的表示

---
+++ src/components/base/NoResult/directive.js
@@ -0,0 +1,4 @@
+import { createDirective } from "../help/createDirective";
+import NoResult from "./index.vue";
+
+export default createDirective(NoResult);
---
+++ src/components/base/NoResult/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="no-result">
+    <div class="no-result-content">
+      <div class="icon"></div>
+      <p class="text">{{ title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoResult",
+};
+</script>
+
+<script setup>
+import { defineExpose, ref } from "vue";
+
+const title = ref("抱歉，没有结果");
+/** 设置标题 */
+function setTitle(str) {
+  title.value = str;
+}
+defineExpose({
+  setTitle,
+});
+</script>
+
+<style scoped lang="scss">
+.no-result {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0);
+
+  .no-result-content {
+    text-align: center;
+
+    .icon {
+      width: 86px;
+      height: 90px;
+      margin: 0 auto;
+      background-size: 86px 90px;
+      @include bg-image("no-result");
+    }
+
+    .text {
+      margin-top: 30px;
+      color: $color-text-d;
+      font-size: $font-size-medium;
+    }
+  }
+}
+</style>
